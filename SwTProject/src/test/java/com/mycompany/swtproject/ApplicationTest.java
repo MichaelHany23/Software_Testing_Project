8f614668626d2eaa08692292e76bf0c704302ec9
Michael15603 <167295851+Michael15603@users.noreply.github.com>
2025-11-26 00:53:46 +0200

    Enhance ApplicationTest with more test cases
    Added additional test cases for the Application class, including error handling for user and movie data. Implemented dummy data creation for testing purposes.

--- SwTProject/src/test/java/com/mycompany/swtproject/ApplicationTest.java
+++ SwTProject/src/test/java/com/mycompany/swtproject/ApplicationTest.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.swtproject;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -84,5 +86,98 @@ public class ApplicationTest {
         // Assert lines 
         assertEquals("ERROR: User ID {1234567BA} is wrong" ,lines.get(0));
         assertEquals(lines.size(),1);
+    }
+  
+    @Test
+    public void TC4 () throws Exception
+    {
+
+        MovieFilereader FrM = new MovieFilereader();
+        UserFilereader FrU= new UserFilereader();
+        Application app = new Application (FrM,FrU);
+        BufferedWriter writer;
+        
+        // movie insertion
+        String dummymovies = "TextFile_integrationTest/dummymovies.txt";
+        writer = new BufferedWriter(new FileWriter(dummymovies, false));
+        writer.write("Spiderman Home Coming , SHC124");
+        writer.newLine();
+        writer.write("Action");
+        writer.close();
+        
+        
+        
+        // user insertion
+        String dummyusers = "TextFile_integrationTest/dummyusers.txt";
+        writer = new BufferedWriter(new FileWriter(dummyusers, false)); 
+        
+        writer.write("michael hany , 123456789"); 
+        writer.newLine();
+        writer.write("SHC124");
+        writer.newLine();
+        // same id wrong
+        writer.write("michael sameh , 123456789");
+        writer.newLine();
+        writer.write("SHC124");
+        writer.close();
+
+        String dummyoutput = "TextFile_integrationTest/dummyoutput.txt";
+        
+        app.RecommenderApp(dummymovies, dummyusers, dummyoutput);
+        
+        List<String> lines = Files.readAllLines(Paths.get(dummyoutput));
+        assertEquals("ERROR: User ID {123456789} is wrong" ,lines.get(0));
+        assertEquals(lines.size(),1);
+    }
+    @Test
+    public void TC5 () throws Exception
+    {
+
+        MovieFilereader FrM = new MovieFilereader();
+        UserFilereader FrU= new UserFilereader();
+        Application app = new Application (FrM,FrU);
+        BufferedWriter writer;
+        
+        // movie insertion
+        String dummymovies = "TextFile_integrationTest/dummymovies.txt";
+        writer = new BufferedWriter(new FileWriter(dummymovies, false));
+        writer.write("Spiderman Home Coming , SHC124");
+        writer.newLine();
+        writer.write("Action");
+        writer.newLine();
+        writer.write("Mickey Mouse , MM123");
+        writer.newLine();
+        writer.write("Animation,Family");
+        writer.close();
+        
+        
+        
+        // user insertion
+        String dummyusers = "TextFile_integrationTest/dummyusers.txt";
+        writer = new BufferedWriter(new FileWriter(dummyusers, false)); 
+        
+        writer.write("michael sameh , 123456789"); 
+        writer.newLine();
+        writer.write("MM123");
+        writer.newLine();
+        // same id wrong
+        writer.write("michael hany , 12345678A");
+        writer.newLine();
+        writer.write("SHC124");
+        writer.close();
+
+        String dummyoutput = "TextFile_integrationTest/dummyoutput.txt";
+        
+        app.RecommenderApp(dummymovies, dummyusers, dummyoutput);
+        
+        List<String> lines = Files.readAllLines(Paths.get(dummyoutput));
+        
+        // no any common genres 
+        assertEquals("michael sameh,123456789" ,lines.get(0));
+        assertEquals("" ,lines.get(1));
+        assertEquals("" ,lines.get(2));
+        assertEquals("michael hany,12345678A" ,lines.get(3));
+        assertEquals("" ,lines.get(4));
+   
     }
 }
